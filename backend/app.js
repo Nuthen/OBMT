4e6b961ac60ebc875899fc46164c678a88ef6f62
SummerCoder11 <51940992+SummerCoder11@users.noreply.github.com>
2019-11-28 19:37:09 -0500

    Backend login queries

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,143 @@ const app = express();
                 res.redirect('/');
             }
             console.log(result);
-            //res.render('index.ejs', {title: Welcome to Socka | View Players,players: result});
         });
     }
 //};
+
+function loginValidation(){
+    var userName = 'Jo';
+    var password = '1234';
+    
+    var loginReport;
+    
+    var userQuery = "SELECT * FROM `user` WHERE Username = '" + userName + "'";
+    
+    db.query(userQuery, (error, results) => {
+        if (error) {
+            //res.redirect('/');
+            loginReport = [{
+            status:false,
+                  message:"There are some error with query"
+            //status = false;
+            //console.log('There are some error with query');
+            }];
+        }
+    
+        else if(results.length >0){
+            //console.log(results);
+            if(password==results[0].Password){
+                //res.redirect('/');
+                loginReport = [{
+                    status:true,
+                    message:"Successfully authenticated"
+                }];
+            }
+                
+            else{
+                //res.redirect('/');
+                loginReport = [{
+                    status:false,
+                      message:"Username and password does not match"
+                
+                }]; 
+            }
+        }
+        
+        else{
+            //res.redirect('/');
+            loginReport = [{
+                status:false,
+                message:"Username does not exits"
+            }];
+        }
+        
+        console.log(loginReport);
+    });
+}
+    
+function registrationValidation(){
+    var UID = 111;
+    var Username = "nicetry"; 
+    var Password = "test";
+    var FName = "joke";
+    var LName = "ster";
+    
+    var dateObj = new Date();
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+    
+    var newdate = year + "/" + month + "/" + day;
+    
+    //var RegDate = new Date();
+    var Admin = true;
+    
+    var registerReport;
+    
+    var userQuery = "SELECT * FROM `user` WHERE Username = '" + Username + "'";
+    
+    db.query(userQuery, (error, results) => {
+        if (error) {
+            //res.redirect('/');
+            registerReport = [{
+                status:false,
+                message:"There are some error with query"
+            //status = false;
+            //console.log('There are some error with query');
+            }];
+        }
+    
+        else if(results.length >0){
+            console.log(results);
+            //if(password==results[0].Password){
+                //res.redirect('/');
+            registerReport = [{
+                status:false,
+                message:"User name is already taken. Please enter another username."
+            }];
+        //} 
+        }
+            
+        else{
+             //res.redirect('/');
+            
+            //var myobj = { UID: 111, Username: "nicetry", Password: "test", FName: "joke", LName: "ster", RegDate: 2019-11-04, Admin: 3};
+            
+            //var myobj2 = [ '111', 'nicetry', 'test', 'joke', 'ster', '2019-11-04', '3'];
+             
+            var insertUser = "INSERT INTO user(UID, Username, Password, FName, LName, RegDate, Admin) VALUES (" + UID + ", '" + Username + "', '" + Password + "', '" + FName + "', '" + LName + "', " + newdate + ", " + Admin + ")";
+            
+            //var insertUser = "INSERT INTO `user` SET "; 
+            
+            console.log(insertUser);
+            //db.collection("user").insertOne(myobj, (err, res) {    
+            //db.query(insertUser, (err2, inserted) => {
+            db.query(insertUser, (err2, inserted) => {
+                if (err2) {
+                    //throw err;
+                    registerReport = [{
+                        status:false,
+                        message:"Error registering."
+                    }];
+                }
+             
+                else{
+                    registerReport = [{
+                        status:true,
+                        message:"Thank you! You are successfully registered."
+                    }];
+                }
+    
+                
+            });
+        }
+        console.log(registerReport);
+     });
+}
+
+
+
 
 
 // create connection to database
@@ -42,6 +175,8 @@ db.connect((err) => {
     console.log('Connected to database');
     
     getHomePage();
+    loginValidation();
+    registrationValidation()
 });
 
 global.db = db;
