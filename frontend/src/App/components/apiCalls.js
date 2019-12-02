04d43cb2711cbc7748dec9569c4f0b5c072d0d80
a-gasior <abgasior@umich.edu>
2019-12-01 23:50:53 -0500

    calls for getting and adding bookmarks

--- frontend/src/App/components/apiCalls.js
+++ frontend/src/App/components/apiCalls.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 
 // PROTOTYPE FOR BACKEND CALL
 export function doSomething() {
-        axios.get(`/api/getList`)
-          .then(res => {
-            console.log(res.data)    
-    })
+    axios.get(`/api/getList`)
+        .then(res => {
+            console.log(res.data)
+        })
 }// END PROTOTYPE
 
 
@@ -20,16 +20,16 @@ export function CallLogin(name, pw) {
         username: name,
         password: pw
     })
-    .then(res => {
-        if(res.data[0] == true){
-            UserId = res.data[1];
-            return true;
-        }
-        else {
-            //displayError
-            return false;
-        }  
-    })
+        .then(res => {
+            if (res.data[0] == true) {
+                UserId = res.data[1];
+                return true;
+            }
+            else {
+                //displayError
+                return false;
+            }
+        })
 }
 
 
@@ -41,20 +41,21 @@ export function CallRegisterLogin(name, pw, dob) {
         password: pw,
         birthdate: dob
     })
-    .then(res => {
-        if(res.data[0] == true){
-            UserId = res.data[1];
-            return true;
-        }
-        else {
-            //displayError
-            return false;
-        }    
-    })
+        .then(res => {
+            if (res.data[0] == true) {
+                UserId = res.data[1];
+                return true;
+            }
+            else {
+                //displayError
+                return false;
+            }
+        })
 }
 
 // ADD/EDIT CALL
 export function CallRegisterBookmark(bid, t, u, p, desc, d, tag) {
+    console.log('good')
     axios.post(`/api/modify`, {
         userId: UserId,
         bid: builtinModules,
@@ -65,16 +66,17 @@ export function CallRegisterBookmark(bid, t, u, p, desc, d, tag) {
         date: d,
         tags: tag
     })
-    .then(res => {
-        if(res.data[0] == true){
-            //displaySuccess
-            return true;
-        }
-        else {
-            //displayError
-            return false;
-        }    
-    })
+        .then(res => {
+            if (res.data[0] == true) {
+                //displaySuccess
+                return true;
+            }
+            else {
+                console.log('fail')
+                //displayError
+                return false;
+            }
+        })
 }
 
 //DELETE CALL
@@ -83,16 +85,16 @@ export function CallDelete(bid) {
         userId: UserId,
         bid: builtinModules
     })
-    .then(res => {
-        if(res.data[0] == true){
-            //displaySuccess
-            return true;
-        }
-        else {
-            //displayError
-            return false;
-        }    
-    })
+        .then(res => {
+            if (res.data[0] == true) {
+                //displaySuccess
+                return true;
+            }
+            else {
+                //displayError
+                return false;
+            }
+        })
 }
 
 //SEARCH CALL
@@ -101,14 +103,47 @@ export function CallSearch(searchString) {
         userId: UserId,
         search: searchString
     })
-    .then(res => {
-        if(res.data[0] == true){
-            //handle returned shit here
-            return res.data; // returning data array (?) -- Might cause Issues still because 1st field is checked for true / false
-        }
-        else {
-            //displayError
-            return false;
-        }     
+        .then(res => {
+            if (res.data[0] == true) {
+                //handle returned shit here
+                return res.data; // returning data array (?) -- Might cause Issues still because 1st field is checked for true / false
+            }
+            else {
+                //displayError
+                return false;
+            }
+        })
+}
+
+export function addBookmark(uid,title,url,description,tags) {
+    axios.post(`/api/addBookmark`, {
+        UID: uid,
+        Title: title,
+        URL: url,
+        Description: description,
+        Tags: tags
     })
+        .then(res => {
+            if (res.data.success == 1) {
+                console.log('it worked');
+            }
+            else {
+                console.log('it didn\'t work');
+            }
+        })
+}
+
+
+export function getBookmarks() {
+    axios.post(`/api/getBookmarks`, {
+        UID:0
+    })
+        .then(res => {
+            if (res.data.success == 1) {
+                console.log('it worked');
+            }
+            else {
+                console.log('it didn\'t work');
+            }
+        })
 }
