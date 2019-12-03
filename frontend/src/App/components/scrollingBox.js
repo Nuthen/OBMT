871f316733d08b6f854313594f2d3aab2c3613e9
a-gasior <abgasior@umich.edu>
2019-12-03 00:30:29 -0500

    adjust var names

--- frontend/src/App/components/scrollingBox.js
+++ frontend/src/App/components/scrollingBox.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
+
+
 export class Bookmark {
-    constructor(title, url, description) {
-        this.title = title;
-        this.url = url;
-        this.description = description;
+    constructor(Title, URL, Description) {
+        this.Title = Title;
+        this.URL = URL;
+        this.Description = Description;
     }
 }
 
 function User(props) {
     return (
-        <div style={{ display: 'flex',justifyContent:'space-evenly'}}>{props.title}</div>
+        <div style={{ display: 'flex',justifyContent:'space-evenly'}}>
+        {props.Title}
+        </div>
     );
 }
 
 function TextBody(props) {
     return (
         <div style={{ display: 'flex',flexDirection:'column', width:'90%',border: 'dashed',borderRadius:'25px' }}>
-            <p>{props.url}</p>
-            <p>{props.description}</p>
+            <p>{props.URL}</p>
+            <p>{props.Description}</p>
         </div>
     );
 }
@@ -35,9 +39,9 @@ function CommentBubble(props) {
 export function CommentBox(props) {
     return (
         <ul style={{ display: 'flex', width: '100%', flexDirection: 'column', listStyleType: 'none', overflowY: 'scroll',backgroundColor:'#629e1d',alignItems:'center',borderRadius:'25px',scrollBehavior:'smooth'}}>{
-            props.map(bookmark => {
-                const ref = React.createRef()
-                return (<li key={bookmark.id} ref={ref} style={{width:'80%'}} ><br/>{CommentBubble(bookmark)}<br /></li>)})
+            props.map(itm => {
+                const ref = React.createRef();
+                return (<li key={itm.id} ref={ref} style={{width:'80%'}} ><br/>{CommentBubble(itm)}<br /></li>)})
         }</ul>
     );
 }
